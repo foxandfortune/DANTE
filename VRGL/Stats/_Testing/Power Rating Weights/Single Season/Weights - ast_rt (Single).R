6744be4b9dffb079fe6e97cc98e5d7b9c259b4ef
Mat Conner <86697388+foxandfortune@users.noreply.github.com>
2025-06-12 17:09:41 -0500

    Test weights for assist rate

--- VRGL/Stats/_Testing/Power Rating Weights/Single Season/Weights - ast_rt (Single).R
+++ VRGL/Stats/_Testing/Power Rating Weights/Single Season/Weights - ast_rt (Single).R
@@ -12,7 +12,7 @@ setwd('..')
 setwd('..')
 
 # Set season/country/tier
-season <- 2024
+season <- 2025
 
 # Negate in formula
 `%!in%` = Negate(`%in%`)
@@ -64,7 +64,7 @@ list <- c('game_date', 'team_id', 'opp_id')
 wgt_var <- 0.97
 
 # Set lambda_adj
-lambda_adj <- 0
+lambda_adj <- 0.1
 
 # Create test.summary to start ----------
 test.summary <- data.frame()
