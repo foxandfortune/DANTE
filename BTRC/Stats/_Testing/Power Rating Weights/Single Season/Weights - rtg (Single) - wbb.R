748b33d7443de218375dc178f251c4eec24a94dc
Mat Conner <86697388+foxandfortune@users.noreply.github.com>
2025-07-22 16:43:49 -0500

    Test wbb ratings weights

--- BTRC/Stats/_Testing/Power Rating Weights/Single Season/Weights - rtg (Single) - wbb.R
+++ BTRC/Stats/_Testing/Power Rating Weights/Single Season/Weights - rtg (Single) - wbb.R
@@ -18,9 +18,9 @@ season <- 2025
 `%!in%` = Negate(`%in%`)
 
 # Load teams and summary data -----
-teams <- readRDS(glue::glue("_Helper Files/Team Data/team_database.rds"))
+teams <- readRDS(glue::glue("_Helper Files/Team Data/team_database_wbb.rds"))
 
-summary <- readRDS(glue::glue("VRGL/Stats/Team and Player Stats/Power Ratings/Raw Data/poss_stats_with_types_{season}.rds")) %>% 
+summary <- readRDS(glue::glue("BTRC/Stats/Team and Player Stats - WBB/Power Ratings/Raw Data/poss_stats_with_types_wbb_{season}.rds")) %>% 
   with_groups(game_id, mutate, tm_ct = n()) %>%
   # Only include games where both teams are in teams dataset
   filter(tm_ct == 2) %>% 
@@ -55,7 +55,7 @@ stat_est <- "rtg"
 
 # Load prior results
 res_all <- readRDS(glue::glue(
-  "VRGL/Stats/_Testing/Power Rating Weights/_Results/single_season_{stat_name}_res.rds"))
+  "BTRC/Stats/_Testing/Power Rating Weights/_Results/single_season_wbb_{stat_name}_res.rds"))
 
 print(res_all %>% 
         arrange(rmse, desc(corr)))
@@ -66,10 +66,10 @@ list <- c('game_date', 'team_id', 'opp_id',
           'is_home', 'neutral_site', 'days_rest', 'travel')
 
 # Set weight for testing; will run for weights 0.90 through 1.00 to start -----
-wgt_var <- 0.97
+wgt_var <- 0.99
 
 ## Set the lambda adj ----------------
-lambda_adj <- 0.95
+lambda_adj <- 0.1
 
 # Create test.summary to start ----------
 test.summary <- data.frame()
@@ -297,8 +297,6 @@ for(i in 1:length(dates$game_date)){
   
   print(i)
   
-  rm(test.date, raw.coeff, base, hfa, tm_ratings, test.data,
-     miles, neutral, rest)
   
   #Sys.sleep(0.5)
 }
@@ -342,13 +340,12 @@ print(res_best)
 ## Results -------------
 saveRDS(res_all,
         glue::glue(
-          "VRGL/Stats/_Testing/Power Rating Weights/_Results/single_season_{stat_name}_res.rds"))
+          "BTRC/Stats/_Testing/Power Rating Weights/_Results/single_season_wbb_{stat_name}_res.rds"))
 
 ## Weights (for creating power ratings) ------------
 saveRDS(res_best,
         glue::glue(
-          "VRGL/Stats/Team and Player Stats/Power Ratings/Weights/single_season_{stat_name}_wgt.rds"))
-
+          "BTRC/Stats/Team and Player Stats - WBB/Power Ratings/Weights/single_season_wbb_{stat_name}_wgt.rds"))
 
 
 
@@ -387,7 +384,7 @@ summary(model)
 
 data.diff %>%
   mutate(est_diff_13 = 1.3 * est_diff,
-         est_diff_116 = 1.16 * est_diff) %>%
+         est_diff_116 = 1.1 * est_diff) %>%
   summarise(
     cor_13 = Metrics::rmse(est_diff_13, act_diff),
     cor_116 = Metrics::rmse(est_diff_116, act_diff),
